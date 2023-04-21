e066bb01a6e639590752c4929f4a407c9d385f6b
faizbyp <faizbyp@gmail.com>
2023-04-22 01:27:52 +0700

    create and read works

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,42 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+import { v4 as uuid } from 'uuid';
+import PreactLogo from './assets/preact.svg'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (input !== '') {
+      setTodos([...todos,
+        {
+          id: uuid(),
+          value: input.trim()
+        }
+      ]);
+    }
+    setInput('');
+  }
 
   return (
     <>
-      <nav className='text-center py-5 bg-primary-subtle'>Todo List</nav>
+      <nav className='text-center py-3 bg-primary text-white'>
+        <img src={PreactLogo} alt="" />
+      </nav>
       <div className="container">
+        <form className='row py-3' onSubmit={handleSubmit}>
+          <input className='text-center mb-2' value={input} type="text" onChange={(e) => {setInput(e.target.value)}} />
+          <button className='btn btn-primary text-white' type="submit">Add Task</button>
+        </form>
+
         <div className="row">
           <div className="col">
-            Hello
+            <ul className="todo-list">
+              {todos.map((todo) => (
+                <li key={todo.id}>{todo.value}</li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './css/main.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
 
 render(<App />, document.getElementById('app'))
