98f1c1a757cbe4aa71231d864d63e777a9e93dbd
faizbyp <faizbyp@gmail.com>
2023-04-22 12:36:02 +0700

    edit works

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import PreactLogo from './assets/preact.svg'
 
 export function App() {
   const [input, setInput] = useState('');
+  const [currentTodo, setCurrentTodo] = useState({});
   const [todos, setTodos] = useState(() => {
     const savedTodos = localStorage.getItem('todos');
     return savedTodos ? JSON.parse(savedTodos) : [];
@@ -12,6 +13,22 @@ export function App() {
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
+
+  function handleEdit(todo) {
+    setCurrentTodo({ ...todo });
+  }
+
+  function handleUpdateTodo(id, updatedTodo) {
+    const updatedItem = todos.map((todo) => {
+      return todo.id === id ? updatedTodo : todo;
+    });
+    setTodos(updatedItem);
+  }
+
+  function handleEditFormSubmit(e) {
+    e.preventDefault();
+    handleUpdateTodo(currentTodo.id, currentTodo);
+  }
 
   function handleDelete(id) {
     const removeItem = todos.filter((todo) => {
@@ -26,7 +43,7 @@ export function App() {
       setTodos([...todos,
         {
           id: uuid(),
-          value: input.trim()
+          text: input.trim()
         }
       ]);
     }
@@ -38,22 +55,42 @@ export function App() {
       <nav className='text-center py-3 bg-primary'>
         <img src={PreactLogo} alt="Preact Todo List" />
       </nav>
+
       <div className="container">
         <form className='row py-3 mx-1' onSubmit={handleSubmit}>
-          <input className='text-center mb-2' value={input} type="text" onChange={(e) => {setInput(e.target.value)}} />
-          <button className='btn btn-primary' type="submit">Add Task</button>
+          <input className='form-control form-control-lg text-center my-2' value={input} type="text" onChange={(e) => {setInput(e.target.value)}} />
+          <button className='btn btn-lg btn-primary' type="submit">Add Task</button>
         </form>
-
         <div className="row">
           <div className="col">
             <ul className="list-unstyled">
               {todos.map((todo) => (
                 <li key={todo.id} className='bg-warning p-2 my-2 rounded'>
-                  {todo.value}
-                  <button className='btn btn-danger ms-2' onClick={() => handleDelete(todo.id)}>X</button>
+                  {todo.text}
+                  <button onClick={() => handleEdit(todo)} type="button" class="btn btn-light ms-2" data-bs-toggle="modal" data-bs-target="#editModal">
+                    Edit
+                  </button>
+                  <button type='button' className='btn btn-danger ms-2' onClick={() => handleDelete(todo.id)}>X</button>
                 </li>
               ))}
             </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Task</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form onSubmit={handleEditFormSubmit}>
+                <input className='form-control form-control-lg text-center my-2 w-100' value={currentTodo.text} type="text" onChange={(e) => setCurrentTodo({...currentTodo, text: e.target.value})} />
+                <button type="submit" class="btn btn-lg btn-primary w-100" data-bs-dismiss="modal">Save changes</button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
