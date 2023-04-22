a016796a08be794304e14fcd21608a30bd3529d6
faizbyp <faizbyp@gmail.com>
2023-04-22 10:19:31 +0700

    task saved in localstorage

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'preact/hooks'
+import { useState, useEffect } from 'preact/hooks'
 import { v4 as uuid } from 'uuid';
 import PreactLogo from './assets/preact.svg'
 
 export function App() {
   const [input, setInput] = useState('');
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  useEffect(() => {
+    // localstorage only support storing strings as keys and values
+    // - therefore we cannot store arrays and objects without converting the object
+    // into a string first. JSON.stringify will convert the object into a JSON string
+    localStorage.setItem("todos", JSON.stringify(todos));
+    // add the todos as a dependancy because we want to update
+    // localstorage anytime the todos state changes
+  }, [todos]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -25,7 +37,7 @@ export function App() {
         <img src={PreactLogo} alt="" />
       </nav>
       <div className="container">
-        <form className='row py-3' onSubmit={handleSubmit}>
+        <form className='row py-3 mx-2' onSubmit={handleSubmit}>
           <input className='text-center mb-2' value={input} type="text" onChange={(e) => {setInput(e.target.value)}} />
           <button className='btn btn-primary text-white' type="submit">Add Task</button>
         </form>
