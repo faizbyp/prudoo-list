5beb9df4ca3c928ddb655f4ca3aaae3a9e16c6f2
faizbyp <faizbyp@gmail.com>
2023-04-22 13:18:21 +0700

    finish

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import { render } from 'preact'
 import { App } from './app.jsx'
 import './css/main.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
 render(<App />, document.getElementById('app'))
