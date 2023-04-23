f1a8738001cf9c92ccad403fd702910ab11deca0
faizbyp <faizbyp@gmail.com>
2023-04-24 06:20:46 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -6,40 +6,40 @@ const manifestForPlugin = {
   registerType: "prompt",
   includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
   manifest: {
-    "name": "Prudoo List",
-    "short_name": "Prudoo List",
-    "description": "Simple todo list using Preact and localStorage",
-    "icons": [
+    name: "Prudoo List",
+    short_name: "Prudoo List",
+    description: "Simple todo list using Preact and localStorage",
+    icons: [
       {
-        src: "/manifest-icon-192.maskable.png",
+        src: "manifest-icon-192.maskable.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any"
       },
       {
-        src: "/manifest-icon-192.maskable.png",
+        src: "manifest-icon-192.maskable.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable"
       },
       {
-        src: "/manifest-icon-512.maskable.png",
+        src: "manifest-icon-512.maskable.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any"
       },
       {
-        src: "/manifest-icon-512.maskable.png",
+        src: "manifest-icon-512.maskable.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable"
       }
     ],
-    "theme_color": "#643cbc",
-    "background_color": "#ffffff",
-    "display": "standalone",
-    "scope": "/",
-    "start_url": "/"
+    theme_color: "#643cbc",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/",
+    start_url: "/"
   }
 }
 
