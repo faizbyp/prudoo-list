afe99c89cc249e52fe90696ee3c5bdd671bc87db
faizbyp <faizbyp@gmail.com>
2023-06-03 21:34:18 +0700

    fix navbar color

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/manifest-icon-512.maskable.png"
 function Landing() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary">
+      <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
           <a class="navbar-brand fw-bold text-white" href="#">Prudoo List</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
